691ce5d22e587ad09a854a8e997d271d0a1d3393
Ayush Gupta <96309609+Ayushhgupta39@users.noreply.github.com>
2022-09-27 23:09:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+var N = document.querySelectorAll(".drum").length;  
+var sounds = ["tom-1.mp3", "tom-2.mp3", "tom-3.mp3", "tom-4.mp3", "snare.mp3", "crash.mp3", "kick-bass.mp3"];
+
+for(let i=0; i<N; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var soundSource = "sounds/" + sounds[i];
+        var audio = new Audio(soundSource);
+        audio.play();
+        
+        var expression = this.innerHTML;
+        makeSound(expression);
+
+       
+
+
+    });
+}
+
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();   
+            break;
+        
+        case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();   
+            break;
+
+        case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();   
+            break;
+
+        case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();   
+            break;
+        
+        case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();   
+            break;
+
+        case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();   
+            break;
+
+        case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();   
+            break;
+
+        default: console.log(key);
+        break;
+    }
+}
+
+
+
+
